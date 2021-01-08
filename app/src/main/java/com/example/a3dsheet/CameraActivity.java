784720affe5966e5d4c8cf9bb180f8b5796b7369
Christo1414 <71657507+Christo1414@users.noreply.github.com>
2021-01-08 11:15:06 -0800

    improved

--- app/src/main/java/com/example/a3dsheet/CameraActivity.java
+++ app/src/main/java/com/example/a3dsheet/CameraActivity.java
@@ -6,44 +6,45 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class CameraActivity extends AppCompatActivity {
 
-    /** Views and Layouts */
+    // Views and Layouts
     private TextView connection_status;
+
+        /** snip this */
     private TextView warning_text;
     private RelativeLayout map_layout;
     private View map_cursor;
     private RelativeLayout zoombar_layout;
     private View zoombar_fill;
+
     private View TranslationView;
-    private View ZoomView;
-    private WebView Camera_WebView;
+        /** snip this */
 
-    /** Variables */
+    // Variables
     private String ip_address;
-    private float px;
-    private float cursor_x_max;
-    private float cursor_x_min;
-    private float cursor_y_max;
-    private float cursor_y_min;
-    private float zoombar_y_max;
-    private float zoombar_y_min;
 
-    /** Variables for touchlistener ( for updating UI )*/
+        /** snip this */
+    private float cursor_x_range;
+    private float cursor_y_range;
+    private float zoombar_range;
+    float map_x_percentage = (float) 0.5;
+    float map_y_percentage = (float) 0.5;
+    float zoom_percentage = (float) 0;
+        /** snip this */
+
+
+
+    // Variable for touch listener ( not needed )
     float x_0 = 0;          // initial values for x and y
     float y_0 = 0;
     float x = 0;            // Coordinates for touch listener
     float y = 0;
-    float i = 0;            // for counting Motion Events
-    float map_x_percentage = (float) 0.5;
-    float map_y_percentage = (float) 0.5;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,129 +52,95 @@ public class CameraActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_camera);
 
+            /** snip this */
         Initialize_Views();
         Initialize_UI();
-        Initialize_WebView();
+            /** snip this */
 
         Translation_Touch_Listener();
         Update_Zoombar();
     }
 
+    private void Initialize_Views(){           /** need this */
 
-    private void Initialize_Views(){
-        // initialize zooms and layouts here
-        connection_status = (TextView) findViewById(R.id.IP_Status);
+
         warning_text = (TextView) findViewById(R.id.Warnings_text);
         map_layout = (RelativeLayout) findViewById(R.id.Map_Layout);
         map_cursor = (View) findViewById(R.id.Map_Cursor_View);
+
         zoombar_layout = (RelativeLayout) findViewById(R.id.ZoomBarLayout);
         zoombar_fill = (View) findViewById(R.id.ZoomBarView);
-        TranslationView = (View) findViewById(R.id.TranslationView);
-        ZoomView = (View) findViewById(R.id.ZoomBarLayout);
-        Camera_WebView = (WebView) findViewById(R.id.Camera_WebView);
 
-        // Set content of IP_Status
+        TranslationView = (View) findViewById(R.id.TranslationView);
+
+
+        // Set content of IP_Status (MAY NOT BE NEEDED)
+        connection_status = (TextView) findViewById(R.id.IP_Status);
         ip_address = getIntent().getStringExtra("EXTRA_IP_ADDRESS");
         connection_status.setText("Connecting to " + ip_address);
 
     }
-    private void Initialize_UI() {
-        // Initialize map cursor (values depend on height and width of map and map cursor)
-        // map size and values are static
-        px = pxFromDp(this, 40);
-        map_cursor.setX(px);
-        map_cursor.setY(px*(float)0.8);
+
+    private void Initialize_UI() {          /** need this */
+     // Map and Zoom UI have static dimensions
+
         // Boundary Conditions
-        cursor_x_max = pxFromDp(this, 80);
-        cursor_x_min = 0;
-        cursor_y_max = pxFromDp(this, 60);
-        cursor_y_min = 0;
+        cursor_x_range = pxFromDp(this, 80);
+        cursor_y_range = pxFromDp(this, 60);
+
+        map_cursor.setX((cursor_x_range)*map_x_percentage);
+        map_cursor.setY((cursor_y_range)*map_y_percentage);
+
 
         // Initialize zoombar
-        // zoombar height is static
-        zoombar_y_max = pxFromDp(this, 500);
-        zoombar_fill.setY(zoombar_y_max);
-        zoombar_y_min = 0;
+        zoombar_range = pxFromDp(this, 500);
+        zoombar_fill.setY((zoombar_range));
+
 
         // Hiding warning textview
         warning_text.setVisibility(View.GONE);
-
     }
 
-    private void Initialize_WebView(){
-        /** CAN DELETE WEBVIEW CODE */
-        // Initialize webview
-        Camera_WebView.setWebViewClient(new WebViewClient());
-        Camera_WebView.getSettings().setJavaScriptEnabled(true);
-        Camera_WebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-        Camera_WebView.getSettings().setPluginState(WebSettings.PluginState.ON);
-        Camera_WebView.getSettings().setMediaPlaybackRequiresUserGesture(false);
-        Camera_WebView.setWebChromeClient(new WebChromeClient());
-        Camera_WebView.loadUrl("https://www.youtube.com/watch?v=8ayK3odtP8M");
-
-
-    }
-
-    public void Update_Zoombar(){
+    public void Update_Zoombar(){           /** need this */
         // Call when zooming
         zoombar_layout.setVisibility(View.VISIBLE);
 
-        /** Update Zoom percentage HERE */
-        float zoom_percentage = (float) 0.5;
+        /** Update ZOOM percentage HERE with global variable or parameter */
+        zoom_percentage = (float) 0.5;
 
-        zoombar_fill.setY((zoombar_y_max - zoombar_y_min)*zoom_percentage);
+        zoombar_fill.setY((zoombar_range)*(1-zoom_percentage));
 
     }
 
-    private void Update_Map(){
-        // Call when panning
+    private void Update_Map(){              /** need this */
+        // Call when translating
         map_layout.setVisibility(View.VISIBLE);
-        Hide_Warning();
+        warning_text.setVisibility(View.GONE); //hide warning
 
-        /** Update map percentages HERE */
+        /** Update MAP percentage HERE with global variable or parameter */
         map_x_percentage = Check_Map_Boundary(map_x_percentage);
         map_y_percentage = Check_Map_Boundary(map_y_percentage);
 
-        map_cursor.setX((cursor_x_max-cursor_x_min)*map_x_percentage);
-        map_cursor.setY((cursor_y_max-cursor_y_min)*map_y_percentage);
+        map_cursor.setX((cursor_x_range)*map_x_percentage);
+        map_cursor.setY((cursor_y_range)*map_y_percentage);
 
 
     }
 
-    private float Check_Map_Boundary(float percentage){
+    private float Check_Map_Boundary(float percentage){             /** need this */
         // for Update_Map
         if (percentage >= 1){
-            Show_Warning();
+            warning_text.setVisibility(View.VISIBLE); //show warning
             percentage =  1;
         }
         else if (percentage <= 0){
-            Show_Warning();
+            warning_text.setVisibility(View.VISIBLE); //show warning
             percentage =  0;
         }
         return percentage;
     }
 
 
-    public void Hide_Map(View view){
-        /** should be called after camera is stationary for some time */
-        map_layout.setVisibility(View.GONE);
-    }
-
-    public void Hide_Zoombar(View view){
-        /** should be called sometime after zooming */
-        zoombar_layout.setVisibility(View.GONE);
-    }
-
-    public void Hide_Warning(){
-        /** should be called sometime after warning appears */
-        warning_text.setVisibility(View.GONE);
-    }
-
-
-    private void Show_Warning(){
-        // called when boundary condition met
-        warning_text.setVisibility(View.VISIBLE);
-    }
 
 
     public void Show_UI(View view){
@@ -192,7 +159,8 @@ public class CameraActivity extends AppCompatActivity {
 
 
     private void Translation_Touch_Listener(){
-        /** NOT FOR FINAL PROJECT. Instead just need to update map_percentage then call update_map()*/
+        /** NOT FOR FINAL PROJECT. Instead just need to update map_percentage then call update_map()
+          same for zoom*/
 
         TranslationView.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -202,7 +170,6 @@ public class CameraActivity extends AppCompatActivity {
                 if(event.getAction() == MotionEvent.ACTION_DOWN) {
                     x_0 = x;
                     y_0 = y;
-                    i = 0;
                 }
                 //x//
                 map_x_percentage = map_x_percentage + (x-x_0)/1000;
@@ -211,7 +178,6 @@ public class CameraActivity extends AppCompatActivity {
                 //x//
                 x_0 = x;
                 y_0 = y;
-                i++;
                 return true;
             }
         });
@@ -222,7 +188,7 @@ public class CameraActivity extends AppCompatActivity {
         return px / context.getResources().getDisplayMetrics().density;
     }
 
-    public static float pxFromDp(final Context context, final float dp) {
+    public static float pxFromDp(final Context context, final float dp) {           /** need this */
         // To find boundary conditions
         return dp * context.getResources().getDisplayMetrics().density;
     }
